29d875cf40e783a835b65706448dc2d244034d11
Samantha Thorne <150044477+13Thornes@users.noreply.github.com>
2024-06-19 16:24:21 -0230

    Added and tested Periodical class

--- Demo.java
+++ Demo.java
@@ -2,16 +2,17 @@ public class Demo {
     public static void main(String[] args) {
         Author a1 = new Author("Cindy Newman", "12-01-1925");
         Book b1 = new Book(123, "Hello", a1, "A2H357", "Publish", 2, "EBook", "Fiction");
+        Periodical p1 = new Periodical(456, "Bye", a1, "1h4s9o", "Company", 3, "Magazine", "45");
 
-        System.out.println(b1.toString());
+        System.out.println(p1.toString());
 
-        b1.Borrow();
+        p1.Borrow();
 
-        System.out.println(b1.toString());
+        System.out.println(p1.toString());
 
-        b1.Return();
+        p1.Return();
 
-        System.out.println(b1.toString());
+        System.out.println(p1.toString());
 
 
     }
