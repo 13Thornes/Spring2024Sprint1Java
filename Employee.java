0e089accc8225329fa9ca794f3612c1fe136b434
Samantha Thorne <150044477+13Thornes@users.noreply.github.com>
2024-06-26 17:31:32 -0230

    Revert "Merge pull request #22 from 13Thornes/Javadocs"
    This reverts commit 079f8a70633c5f81bde5f1f6929d3d2bc2a7a9a5, reversing
    changes made to ee64473997402293747dafa9f50c5ed8b7d33ed2.

--- Employee.java
+++ Employee.java
@@ -1,45 +1,19 @@
 import java.util.ArrayList;
 /**
  * @author Samantha Throne
- * @version 1.00
+ * @version
  */
 public class Employee  extends Patron{
-    /**
-     * repersents the id of the employee
-     */
-
+    
     private int empID;
-
-    /**
-     * repersents the postion of the employee
-     */
     private String position;
 
-    /**
-     * Create an Employee object
-     * @param patronID accepts the patronID and set it to the Employee object
-     * @param name accepts the name and set it to the Employee object
-     * @param address accepts the address and set it to the Employee object
-     * @param phoneNum accepts the phone number and set it to the Employee objects
-     * @param empID accepts the empID and set it to the Employee object
-     * @param position accepts the position and set it to the Employee object
-     */
     public Employee(int patronID, String name, String address, String phoneNum, int empID, String position) {
         super(patronID, name, address, phoneNum);
         this.empID = empID;
         this.position = position;
     }
 
-    /**
-     * Create an Employee object
-     * @param patronID accepts the patronID and set it to the Employee object
-     * @param name accepts the name and set it to the Employee object
-     * @param address accepts the address and set it to the Employee object
-     * @param phoneNum accepts the phone number and set it to the Employee objects
-     * @param empID accepts the empID and set it to the Employee object
-     * @param position accepts the position and set it to the Employee object
-     * @param itemList accepts the itemList and set it to the Employee object
-     */
     public Employee(int patronID, String name, String address, String phoneNum, int empID, String position, ArrayList<LibaryItem> itemList) {
         super(patronID, name, address, phoneNum);
         this.empID = empID;
@@ -48,42 +22,23 @@ public class Employee  extends Patron{
         borrowList.addAll(itemList);
     }
 
-    /**
-     * 
-     * @return Returns the empID of the Employee Object
-     */
     public int getEmpID() {
         return empID;
     }
 
-    /**
-     * 
-     * @param empID accepts the empID and set it to the Employee object
-     */
     public void setEmpID(int empID) {
         this.empID = empID;
     }
 
-    /**
-     * 
-     * @return returns the position of the Employee object
-     */
     public String getPosition() {
         return position;
     }
 
-    /**
-     * 
-     * @param position accepts the postion and set it to the Employee object
-     */
     public void setPosition(String position) {
         this.position = position;
     }
 
 
-    /**
-     * @return returns the Employee object data as a string
-     */
     @Override
     public String toString() {
         return (super.toString() + " Employee[EmployeeID= " + empID + ", Position= " + position + "]") ;
--- Student.java
+++ Student.java
@@ -1,35 +1,12 @@
 
 import java.util.ArrayList;
 
-/**
- * @author Samantha Throne
- * @version 1.00
- */
 
 public class Student extends Patron{
-    /**
-     * repersents the ID of the student
-     */
     private int studentID;
-    /**
-     * repersents the school of the student
-     */
     private String school;
-    /**
-     * repersents the grade of the student
-     */
     private int grade;
 
-    /**
-     * creates a Student object
-     * @param patronID accepts patronID and set it to the Student object
-     * @param name accepts name and set it to the Student object
-     * @param address accepts address and set it to the Student object
-     * @param phoneNum accepts phoneNum and set it to the Sutdent object
-     * @param studentID accepts studentID and set it to the Student object
-     * @param school  accepts school and set it to the Student object
-     * @param grade accepts grade and set it to the Student object
-     */
     public Student(int patronID, String name, String address, String phoneNum, int studentID, String school, int grade) {
         super(patronID, name, address, phoneNum);
         this.studentID = studentID;
@@ -37,18 +14,6 @@ public class Student extends Patron{
         this.grade = grade;
     }
 
-
-    /**
-     * creates a Student object
-     * @param patronID accepts patronID and set it to the Student object
-     * @param name accepts name and set it to the Student object
-     * @param address accepts address and set it to the Student object
-     * @param phoneNum accepts phoneNum and set it to the Sutdent object
-     * @param studentID accepts studentID and set it to the Student object
-     * @param school  accepts school and set it to the Student object
-     * @param grade accepts grade and set it to the Student object
-     * @param grade accepts itemList and set it to the Student object
-     */
 
     public Student(int patronID, String name, String address, String phoneNum, int studentID, String school, int grade, ArrayList<LibaryItem> itemList) {
         super(patronID, name, address, phoneNum);
@@ -59,65 +24,30 @@ public class Student extends Patron{
         borrowList.addAll(itemList);
     }
 
-    /**
-     * 
-     * @return returns the studentID of the Student object
-     */
     public int getStudentID() {
         return studentID;
     }
 
-
-
-    /**
-     * 
-     * @param studentID accepts studentID and set it to the Student object
-     */
     public void setStudentID(int studentID) {
         this.studentID = studentID;
     }
 
-
-    /**
-     * 
-     * @return returns the school of the Student object
-     */
     public String getSchool() {
         return school;
     }
 
-
-
-    /**
-     * 
-     * @param school accepts school and set it to the Student object
-     */
     public void setSchool(String school) {
         this.school = school;
     }
 
-
-    /**
-     * 
-     * @return returns the grade of the Student object
-     */
     public int getGrade() {
         return grade;
     }
 
-
-
-    /**
-     * 
-     * @param grade accepts grade and set it to the Student object
-     */
     public void setGrade(int grade) {
         this.grade = grade;
     }
 
-    /**
-     * @return returns all the data of the Student object as a string
-     */
     @Override
     public String toString() {
         return (super.toString() + " Student[StudentID= " + studentID + ", School= " + school + " , Grade= " + grade + "]") ;
